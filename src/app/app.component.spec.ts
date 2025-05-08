3909305e51c8422b67b2c5e5c12fdc6f08beeb71
Joanne Hoar <joanne.hoar@robertsoncollege.ca>
2025-05-08 14:51:37 -0600

    Simplifying/customizing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Welcome to Front End' title`, () => {
+  it(`should have the 'Products App' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Welcome to Front End');
+    expect(app.title).toEqual('Products App');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Welcome to Front End');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Products App');
   });
 });
