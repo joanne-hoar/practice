34e4656c69a648002d9bacc934d12e5974740a07
Joanne Hoar <joanne.hoar@surje.ca>
2025-05-09 12:34:16 -0600

    made products home read data in from a json file

--- src/app/products-home/products-home.component.ts
+++ src/app/products-home/products-home.component.ts
@@ -3,29 +3,24 @@ import { Component } from '@angular/core';
 import { ProductComponent } from '../product/product.component';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products-home',
-  imports: [ProductComponent, HeaderComponent, FooterComponent, CommonModule],
+  imports: [ProductComponent, HeaderComponent, FooterComponent, CommonModule, HttpClientModule],
   templateUrl: './products-home.component.html', 
   styleUrl: './products-home.component.css'
 })
-export class ProductsHomeComponent {
-  products = [
-  {
-    "name": "Can of Beans",
-    "image": "can-of-beans.jpg",
-    "description": "Full of magical moments, this can of beans could be yours!"
-  },
-  {
-    "name": "Bag of Crisps",
-    "image": "bag-of-crisps.jpg",
-    "description": "That should say chips. Any flavour you want."
-  },
-  {
-    "name": "Gummy Bears",
-    "image": "gummy-bears.jpg",
-    "description": "Be sure to specify quantity as we have too many and will send you a lot."
+export class ProductsHomeComponent implements OnInit {
+  products: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any[]>('assets/grocery.json').subscribe((data) => {
+      this.products = data;
+    });
   }
-]
 }
