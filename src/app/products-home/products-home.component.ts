0784617738e279f77ce7d087e7293d25ce97c7ba
Joanne Hoar <joanne.hoar@surje.ca>
2025-05-21 10:50:41 -0600

    fixed lint error

--- src/app/products-home/products-home.component.ts
+++ src/app/products-home/products-home.component.ts
@@ -6,14 +6,22 @@ import { FooterComponent } from '../footer/footer.component';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 
+interface Product {
+  // matches expected json file
+  name: string;
+  image: string;
+  description: string;
+}
+
 @Component({
   selector: 'app-products-home',
   imports: [ProductComponent, HeaderComponent, FooterComponent, CommonModule],
   templateUrl: './products-home.component.html', 
   styleUrl: './products-home.component.css'
 })
+
 export class ProductsHomeComponent implements OnInit {
-  products: any[] = [];
+  products: Product[] = [];
   category?: string | null;
   @ViewChildren(ProductComponent) productComponents!: QueryList<ProductComponent>;
 
@@ -36,7 +44,7 @@ export class ProductsHomeComponent implements OnInit {
   }
 
   sendOrder(): void {
-    var message = "";
+    let message = "";
     this.productComponents.forEach(product => {
      if(product.count > 0)
         message += product.count + ' ' + product.name + ', ';
