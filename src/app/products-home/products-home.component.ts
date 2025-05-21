64deedeba375b214ffab9a421b5ce8e8971a8bcd
Joanne Hoar <joanne.hoar@surje.ca>
2025-05-21 15:21:06 -0600

    fixed lint errors, fixed test errors, and refactored images in assets folder

--- src/app/products-home/products-home.component.ts
+++ src/app/products-home/products-home.component.ts
@@ -3,24 +3,39 @@ import { Component } from '@angular/core';
 import { ProductComponent } from '../product/product.component';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
-import { HttpClient } from '@angular/common/http';
-import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
 import { OnInit } from '@angular/core';
 
+interface Product {
+  // matches expected json file
+  name: string;
+  image: string;
+  description: string;
+  category: string;
+}
+
+/**
+ * ProductsHomeComponent
+ *
+ * Displays a list of products based on the selected category.
+ * Fetches product data from a JSON file, renders each product using ProductComponent,
+ * and allows users to submit their order. Handles navigation and route parameters.
+ */
 @Component({
   selector: 'app-products-home',
-  imports: [ProductComponent, HeaderComponent, FooterComponent, CommonModule, HttpClientModule],
+  imports: [ProductComponent, HeaderComponent, FooterComponent, CommonModule],
   templateUrl: './products-home.component.html', 
   styleUrl: './products-home.component.css'
 })
 export class ProductsHomeComponent implements OnInit {
-  products: any[] = [];
-
-  constructor(private http: HttpClient) {}
+  // Array of products loaded from the JSON file
+  products: Product[] = [];
 
   ngOnInit(): void {
-    this.http.get<any[]>('assets/grocery.json').subscribe((data) => {
-      this.products = data;
-    });
+    fetch('assets/products.json')
+      .then(response => response.json())
+      .then((data: Product[]) => {
+        this.products = data;
+      })
+      .catch(error => console.error('Error fetching JSON:', error));
   }
 }
